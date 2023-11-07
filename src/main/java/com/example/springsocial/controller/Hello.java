70b53b4096a8447358225f2c33752167b9f60218
yuvarajram1591 <149653474+yuvarajram1591@users.noreply.github.com>
2023-11-07 16:19:08 +0530

    Update Hello.java

--- src/main/java/com/example/springsocial/controller/Hello.java
+++ src/main/java/com/example/springsocial/controller/Hello.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-    @RequestMapping("/oauth")
+@RequestMapping("/auth")
 public class Hello {
     @GetMapping("/hello")
     public String hello(){
